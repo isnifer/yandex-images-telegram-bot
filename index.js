29d4fd3a273daca8b834f982f2131d1ec0fe82cd
Anton Kuznetsov <isnifer@gmail.com>
2015-09-22 00:00:12 +0300

    Main file move up

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-import Bot from 'node-telegram-bot';
-import request from 'request';
-import fs from 'fs';
-import http from 'http';
-import jsonfile from 'jsonfile';
-import token from './token';
+'use strict';
 
-const messages = {
-    success: 'Welcome on board! Everything is ok!\nPlease, wait just a moment.\n' +
-        'I will send you "Picture of the Day"',
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _nodeTelegramBot = require('node-telegram-bot');
+
+var _nodeTelegramBot2 = _interopRequireDefault(_nodeTelegramBot);
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _jsonfile = require('jsonfile');
+
+var _jsonfile2 = _interopRequireDefault(_jsonfile);
+
+var _dataToken = require('./data/token');
+
+var _dataToken2 = _interopRequireDefault(_dataToken);
+
+var messages = {
+    success: 'Welcome on board! Everything is ok!\nPlease, wait just a moment.\n' + 'I will send you "Picture of the Day"',
     error: 'Oops, something wrong. Try /subscribe one more time',
     subscribed: 'You\'ve been already successfully subscribed',
     unsubscribed: 'You successfully unsubscribed',
     notFound: 'Sorry, but I haven\'t picture with this id in my collection'
 };
 
-const bot = new Bot(token);
-const file = __dirname + '/users.json';
+var bot = new _nodeTelegramBot2['default'](_dataToken2['default']);
+var file = _path2['default'].resolve(__dirname, 'data', 'users.json');
 
 var users = [];
-jsonfile.readFile(file, function(err, arr) {
+_jsonfile2['default'].readFile(file, function (err, arr) {
     users = arr || [];
 });
 
-function trasformMonth (month) {
+function trasformMonth(month) {
     if (month + 1 < 10) {
         return '0' + (month + 1);
     }
@@ -30,14 +50,14 @@ function trasformMonth (month) {
     return month + 1;
 }
 
-function standardCallback (err) {
+function standardCallback(err) {
     if (err) {
         console.error(err);
     }
     console.log(users);
 }
 
-function sendPhoto (id, imgPath) {
+function sendPhoto(id, imgPath) {
     bot.sendPhoto({
         chat_id: id,
         caption: 'Picture of the Day ' + imgPath.match(/\d/g).join(''),
@@ -47,16 +67,16 @@ function sendPhoto (id, imgPath) {
     });
 }
 
-function sendMessage (id, text) {
+function sendMessage(id, text) {
     bot.sendMessage({
         chat_id: id,
         text: text
     });
 }
 
-function get (id, imgId) {
-    var imgPath = './pics/' + imgId.match(/\d/g).join('') + '.jpg';
-    var picture = fs.createReadStream(imgPath);
+function get(id, imgId) {
+    var imgPath = _path2['default'].resolve(__dirname, 'pics', imgId.match(/\d/g).join('') + '.jpg');
+    var picture = _fs2['default'].createReadStream(imgPath);
 
     picture.on('error', function () {
         sendMessage(id, messages.notFound);
@@ -65,45 +85,42 @@ function get (id, imgId) {
     picture.on('readable', sendPhoto.bind(this, id, imgPath));
 }
 
-function sendPhotoHandler (id) {
-    const today = new Date();
-    const imgPath = './pics/' + today.getFullYear() + trasformMonth(today.getMonth()) + today.getDate() + '.jpg';
-    var file = fs.createReadStream(imgPath);
+function sendPhotoHandler(id) {
+    var today = new Date();
+    var imgPath = _path2['default'].resolve(__dirname, 'pics', today.getFullYear() + trasformMonth(today.getMonth()) + today.getDate() + '.jpg');
+    var stream = _fs2['default'].createReadStream(imgPath);
 
-    file.on('error', function () {
-        request
-            .get('https://yandex.ru/images/today?size=2560x1600')
-            .pipe(fs.createWriteStream(imgPath))
-            .on('finish', sendPhoto.bind(this, id, imgPath));
+    stream.on('error', function () {
+        _request2['default'].get('https://yandex.ru/images/today?size=2560x1600').on('finish', sendPhoto.bind(this, id, imgPath)).pipe(_fs2['default'].createWriteStream(imgPath));
     });
 
-    file.on('readable', sendPhoto.bind(this, id, imgPath));
+    stream.on('readable', sendPhoto.bind(this, id, imgPath));
 }
 
-function subscribe (id) {
+function subscribe(id) {
     if (users.indexOf(id) === -1) {
         users.push(id);
         console.log('==================== USER SUBSCRIBED ====================');
-        jsonfile.writeFile(file, users, standardCallback);
+        _jsonfile2['default'].writeFile(file, users, standardCallback);
         sendMessage(id, messages.success);
 
         // Send today's photo after subscription
         return sendPhotoHandler(id);
     }
+
     return sendMessage(id, messages.subscribed);
 }
 
-function unsubscribe (id) {
-    var index = users.indexOf(id);
-    if (index !== -1) {
+function unsubscribe(id) {
+    if (users.indexOf(id) !== -1) {
         users.splice(index, 1);
         console.log('==================== USER DELETED ====================');
-        jsonfile.writeFile(file, users, standardCallback);
+        _jsonfile2['default'].writeFile(file, users, standardCallback);
         return sendMessage(id, messages.unsubscribed);
     }
 }
 
-function onMessageReceived (message) {
+function onMessageReceived(message) {
     if (message.text === '/subscribe') {
         subscribe(message.from.id);
     } else if (message.text === '/unsubscribe') {
---
+++ src.js
@@ -0,0 +1,126 @@
+import Bot from 'node-telegram-bot';
+import request from 'request';
+import fs from 'fs';
+import path from 'path';
+import jsonfile from 'jsonfile';
+import token from './data/token';
+
+const messages = {
+    success: 'Welcome on board! Everything is ok!\nPlease, wait just a moment.\n' +
+        'I will send you "Picture of the Day"',
+    error: 'Oops, something wrong. Try /subscribe one more time',
+    subscribed: 'You\'ve been already successfully subscribed',
+    unsubscribed: 'You successfully unsubscribed',
+    notFound: 'Sorry, but I haven\'t picture with this id in my collection'
+};
+
+const bot = new Bot(token);
+const file = path.resolve(__dirname, 'data', 'users.json');
+
+var users = [];
+jsonfile.readFile(file, function(err, arr) {
+    users = arr || [];
+});
+
+function trasformMonth (month) {
+    if (month + 1 < 10) {
+        return '0' + (month + 1);
+    }
+
+    return month + 1;
+}
+
+function standardCallback (err) {
+    if (err) {
+        console.error(err);
+    }
+    console.log(users);
+}
+
+function sendPhoto (id, imgPath) {
+    bot.sendPhoto({
+        chat_id: id,
+        caption: 'Picture of the Day ' + imgPath.match(/\d/g).join(''),
+        files: {
+            photo: imgPath
+        }
+    });
+}
+
+function sendMessage (id, text) {
+    bot.sendMessage({
+        chat_id: id,
+        text: text
+    });
+}
+
+function get (id, imgId) {
+    var imgPath = path.resolve(__dirname, 'pics', imgId.match(/\d/g).join('') + '.jpg');
+    var picture = fs.createReadStream(imgPath);
+
+    picture.on('error', function () {
+        sendMessage(id, messages.notFound);
+    });
+
+    picture.on('readable', sendPhoto.bind(this, id, imgPath));
+}
+
+function sendPhotoHandler (id) {
+    const today = new Date();
+    const imgPath = path.resolve(
+        __dirname,
+        'pics',
+        today.getFullYear() + trasformMonth(today.getMonth()) + today.getDate() + '.jpg'
+    );
+    var stream = fs.createReadStream(imgPath);
+
+    stream.on('error', function () {
+        request
+            .get('https://yandex.ru/images/today?size=2560x1600')
+            .on('finish', sendPhoto.bind(this, id, imgPath))
+            .pipe(fs.createWriteStream(imgPath));
+    });
+
+    stream.on('readable', sendPhoto.bind(this, id, imgPath));
+}
+
+function subscribe (id) {
+    if (users.indexOf(id) === -1) {
+        users.push(id);
+        console.log('==================== USER SUBSCRIBED ====================');
+        jsonfile.writeFile(file, users, standardCallback);
+        sendMessage(id, messages.success);
+
+        // Send today's photo after subscription
+        return sendPhotoHandler(id);
+    }
+
+    return sendMessage(id, messages.subscribed);
+}
+
+function unsubscribe (id) {
+    if (users.indexOf(id) !== -1) {
+        users.splice(index, 1);
+        console.log('==================== USER DELETED ====================');
+        jsonfile.writeFile(file, users, standardCallback);
+        return sendMessage(id, messages.unsubscribed);
+    }
+}
+
+function onMessageReceived (message) {
+    if (message.text === '/subscribe') {
+        subscribe(message.from.id);
+    } else if (message.text === '/unsubscribe') {
+        unsubscribe(message.from.id);
+    } else if (message.text.match(/\/get/)) {
+        get(message.from.id, message.text);
+    }
+}
+
+bot.on('message', onMessageReceived);
+
+bot.start();
+
+setInterval(function () {
+    users.forEach(sendPhotoHandler);
+}, 86400000);
